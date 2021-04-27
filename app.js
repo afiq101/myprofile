8138772c209040381e4eeb689fd04b1650984ca8
afiq <mdafiqiskandar@gmail.com>
2021-04-28 07:54:38 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => console.info(`Listening on post 3000`));
+
+app.get("/", (req, res) => {
+  let data = {
+    title: "My Profile",
+  };
+  res.render("index", data);
+});
+
+module.exports = app;
